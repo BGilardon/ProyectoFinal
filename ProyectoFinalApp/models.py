a2baa1df5cbe51d33ef929850e77e32635ddb299
bautista-gilardon <bautistagilardon@gmail.com>
2022-06-24 18:21:02 -0300

    PrimerDia

--- ProyectoFinalApp/models.py
+++ ProyectoFinalApp/models.py
@@ -1,3 +1,23 @@
+import mailbox
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    
+    nombre = models.CharField(max_length=30)
+    mail = models.EmailField()
+    nacimiento = models.DateField()
+
+class Posteo(models.Model):
+    titulo = models.CharField(max_length=20)
+    cuerpo = models.CharField(max_length=200)    
+
+class Moderador(models.Model):
+    nombre = models.CharField(max_length=30)
+    mail = models.EmailField()
+    sector = models.CharField(max_length=30)
+    
+
+
